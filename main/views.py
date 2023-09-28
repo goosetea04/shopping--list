7eb33e130a2832ff7b4e853abbabbf82db4c1192
goosetea04 <gusti.fatu@gmail.com>
2023-09-28 22:38:40 +0700

    finished tutorial 4, makasih ka Ferry

--- main/views.py
+++ main/views.py
@@ -23,7 +23,9 @@ def show_main(request):
     
     context = {
         'name': request.user.username,
-        'class': 'KKI'
+        'class': 'KKI',
+        'last_login': request.COOKIES['last_login'],
+        'products': products,
     }
 
     return render(request, 'main.html', context)
@@ -88,3 +90,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product by ID
+    product = Product.objects.get(pk = id)
+
+    # Set product as instance of form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Save the form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data by ID
+    product = Product.objects.get(pk=id)
+    # Delete data
+    product.delete()
+    # Return to the main page
+    return HttpResponseRedirect(reverse('main:show_main'))
