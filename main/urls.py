5ca529dc48bbe23f992512de9d09bd90feb64f1d
goosetea04 <gusti.fatu@gmail.com>
2023-10-05 09:44:31 +0700

    completed tutorial 5

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from main.views import show_main, create_product, show_xml, show_json
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register
 from main.views import login_user
-from main.views import logout_user, edit_product, delete_product
+from main.views import logout_user, edit_product, delete_product, get_product_json, add_product_ajax
 app_name = 'main'
 
 urlpatterns = [
@@ -18,4 +18,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>/', edit_product, name='edit_product'),
     path('delete/<int:id>/', delete_product, name='delete_product'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.urls import reverse
 from main.forms import ProductForm
 from main.models import Product
@@ -14,6 +14,7 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt 
 
 # Create your views here
 @login_required(login_url='/login')
@@ -113,3 +114,22 @@ def delete_product(request, id):
     product.delete()
     # Return to the main page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
